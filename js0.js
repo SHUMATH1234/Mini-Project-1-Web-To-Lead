7ee67ab200e1c90a65b01a73b351806c1a03c855
SHUMATH1234 <shubhammathurkar1997@gmail.com>
2025-03-07 15:05:59 +0530

    final

--- js0.js
+++ js0.js
@@ -1,30 +1,29 @@
 let captchaCheck = false;
 function beforeSubmit(event){
-//if(captchaCheck){
+if(captchaCheck){
     let outputDate = document.querySelector(".outputData");
     let inputDate = document.querySelector(".inputDate");
     console.log("inputDate.value", inputDate.value);
 
     let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
     outputDate.value = formattedDate;
-// }else{
- //  alert("Please check the reCaptcha Box to Submit the Lead");
- //    event.preventDefault();
-// }
-
+ }else{
+    alert("Please check the reCaptcha Box to Submit the Lead");
+     event.preventDefault();
+ }
 
 }
 
-// function timestamp() {
-//      var response = document.getElementById("g-recaptcha-response"); 
-//      if (response == null || response.value.trim() == "") {
-//         var elems = JSON.parse(
-//             document.getElementsByName("captcha_settings")[0].value
-//         );
-//         elems["ts"] = JSON.stringify(new Date().getTime());
-//         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
-//     } 
-// } 
+function timestamp() {
+     var response = document.getElementById("g-recaptcha-response"); 
+     if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(
+            document.getElementsByName("captcha_settings")[0].value
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    } 
+} 
 setInterval(timestamp, 500);
 
 function captchaSuccess(){
